ceaf271ce2e6734312291d2734fae95419ced334
Jim Dowling <jdowling@sics.se>
2016-08-24 12:50:30 +0200

    code for downloading and installing shibboleth

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,19 @@
-
 include_recipe "apache2"
 
+
+
+
+bash "download_extract_shibboleth" do
+    user "root"
+    code <<-EOF
+
+  wget ....
+  tar zxf ....
+  mkdir ...
+  mv ...
+
+
+
+EOF
+#  not_if { ::File.exists?( "#{node.shibboleth}/.." ) }
+end
